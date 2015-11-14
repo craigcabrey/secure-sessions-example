4f578175ccc7ad76691f6e50701bdc53bb4962d7
Craig Cabrey <craigcabrey@gmail.com>
2015-11-13 23:13:43 -0500

    Add client id to index view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SWEN-755', user: req.user, id: req.session.id });
+  res.render('index', {
+    title: 'SWEN-755', user: req.user, id: req.session.id
+  });
 });
 
 module.exports = router;
